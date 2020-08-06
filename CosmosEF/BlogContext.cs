89d027f07c1663e5ea6e25eb8c2c2d7b0cb3aff1
Jason446620 <panshubei@outlook.com>
2020-08-06 17:44:40 +0800

    Update BlogContext.cs

--- CosmosEF/BlogContext.cs
+++ CosmosEF/BlogContext.cs
@@ -13,7 +13,7 @@ namespace CosmosEF
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseCosmos(
-                    "https://stevesqlapi.documents.azure.com:443/",
+                    "https://stezure.com:443/",
                     "ITRIaAzzXsSamTvbgZLbEhrkDf8VIpuef4X7HlI83wkFaBTcJTRJ6C1sWhUiMfA==",
                     databaseName: "Blog");
 
